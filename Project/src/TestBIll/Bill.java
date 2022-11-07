e8d187cf374c28cd1372d71f13c5d65f6525860c
firstfear1412 <78691900+firstfear1412@users.noreply.github.com>
2022-11-07 15:28:23 +0700

    Bill ที่มีดีไซน์

--- Project/src/TestBIll/Bill.java
+++ Project/src/TestBIll/Bill.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package TestBIll;
+package TestBill;
 //import static TestBill.PT*;
 import java.util.ArrayList;
 /**
